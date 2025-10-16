decbb2910e9decda2c28d80249fd1c7ba5d589a8
AK-viHaAn <kanand2607755@gmail.com>
2025-10-17 04:09:15 +0530

    updated the live backend urL

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:7070";
+const API_BASE = "https://casino-t8a2.vercel.app";
 
 export const api = axios.create({
   baseURL: API_BASE,
